b521709e6c28adba17cf22a35fae8ac2231bdb1e
Ron Martin <ronmartin89@gmail.com>
2020-03-30 12:56:05 -0700

    working on the problem

--- string-splitter.js
+++ string-splitter.js
@@ -4,10 +4,33 @@
  * @param {string} separator 
  */
 function stringSplitter(str, separator) {
+  let newArray = [];
+  if(str.length === 0) {
+    return newArray;
+  } 
+  // 12/01/2020
+  // 1 + stringSplitter(2/01/2020)
+  // 2 + stringSplitter(/01/2020)
+  // /                  01/2020
+
+  const newStr = str[0]; //   12/
+  if (newStr === '/') {  // we can change '/' to separator variable
+    // add newStr to array
+    console.log('SEPARATOR');
+    newArray = [...newStr];    // ["12"] 
+  }
+
+  return newStr + stringSplitter(str.slice(1));
+  // abc
+  // return a + stringSplitter(bc)
+  // retrun b + stringSplitter(c)
+  // return c
+  // return ''
+
 
 }
 
-const result = stringSplitter();
+const result = stringSplitter('12/20/2020');
 console.log(result);
 
 // INPUT: 02/20/2020
@@ -23,17 +46,17 @@ console.log(result);
 // 
 // newString = first letter of string
 // if newString is === /, then skip that element and put the newString in an array  (might want to use ...spreadOperator)
-// if newString is NOT === /, then pass it to
+// if newString is NOT === /, then pass it to stringSplitter
 
 // 02/20/2020
-// 0     RETURN 0
-// 2     RETURN 02
-// '/'   RETURN ["02"]
-// 2     RETURN 2
-// 0     RETURN 0
-// '/'   RETURN ["02", "20"]
-// 2     RETURN 2
-// 0     RETURN 0
-// 2     RETURN 2
-// 0     RETURN 0
+// 0     RETURN 0            02/20/2020        const newStr = str.slice(1)
+// 2     RETURN 02           2/20/2020         returnn newStr + stringSplitter(str.splice(0, 1))
+// '/'   RETURN ["02"]       /20/2020
+// 2     RETURN 2            20/2020
+// 0     RETURN 0            0/2020
+// '/'   RETURN ["02", "20"] /2020
+// 2     RETURN 2            2020
+// 0     RETURN 0            020
+// 2     RETURN 2            20
+// 0     RETURN 0            0
 // end   RETURN ["02", "20", "2020"]     (str.length===0)
