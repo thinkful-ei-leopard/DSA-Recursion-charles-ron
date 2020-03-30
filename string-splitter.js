3b8f0ae554e88a76a00c10e6c1a690f9e5f8179b
Ron Martin <ronmartin89@gmail.com>
2020-03-30 13:54:45 -0700

    working on string splitter

--- string-splitter.js
+++ string-splitter.js
@@ -4,30 +4,20 @@
  * @param {string} separator 
  */
 function stringSplitter(str, separator) {
-  let newArray = [];
   if(str.length === 0) {
-    return newArray;
+    return [];
   } 
-  // 12/01/2020
-  // 1 + stringSplitter(2/01/2020)
-  // 2 + stringSplitter(/01/2020)
-  // /                  01/2020
 
-  const newStr = str[0]; //   12/
+  const newStr = str[0]; 
+  // if newString is === separator, then skip that element and put the newString in an array  (might want to use ...spreadOperator), then continue
   if (newStr === '/') {  // we can change '/' to separator variable
-    // add newStr to array
     console.log('SEPARATOR');
-    newArray = [...newStr];    // ["12"] 
+    console.log(str.slice(1));
+    return [newStr.slice(1), ...stringSplitter(str.slice(1))];
   }
-
+  // if newString is NOT === separator, then pass it to stringSplitter
+  // console.log(newStr);
   return newStr + stringSplitter(str.slice(1));
-  // abc
-  // return a + stringSplitter(bc)
-  // retrun b + stringSplitter(c)
-  // return c
-  // return ''
-
-
 }
 
 const result = stringSplitter('12/20/2020');
@@ -45,18 +35,18 @@ console.log(result);
 // check str[0] = separator ?
 // 
 // newString = first letter of string
-// if newString is === /, then skip that element and put the newString in an array  (might want to use ...spreadOperator)
-// if newString is NOT === /, then pass it to stringSplitter
+
+
 
 // 02/20/2020
 // 0     RETURN 0            02/20/2020        const newStr = str.slice(1)
-// 2     RETURN 02           2/20/2020         returnn newStr + stringSplitter(str.splice(0, 1))
-// '/'   RETURN ["02"]       /20/2020
+// 02    RETURN 02           2/20/2020         returnn newStr + stringSplitter(str.splice(0, 1))
+// 02/   RETURN ["02"]       /20/2020
 // 2     RETURN 2            20/2020
-// 0     RETURN 0            0/2020
-// '/'   RETURN ["02", "20"] /2020
+// 20     RETURN 0            0/2020
+// 20/   RETURN ["02", "20"] /2020
 // 2     RETURN 2            2020
-// 0     RETURN 0            020
-// 2     RETURN 2            20
-// 0     RETURN 0            0
+// 20     RETURN 0            020
+// 202     RETURN 2            20
+// 2020     RETURN 0            0
 // end   RETURN ["02", "20", "2020"]     (str.length===0)
