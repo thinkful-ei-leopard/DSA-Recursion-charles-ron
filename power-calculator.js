838557e1c524e1ddd688ef0c4863a1538402b717
Ron Martin <ronmartin89@gmail.com>
2020-03-30 09:52:13 -0700

    working on examplle 2

--- power-calculator.js
+++ power-calculator.js
@@ -2,25 +2,26 @@
 function powerCalculator(base, exponent){
   // base case
   if( exponent < 0){
-    return console.log('exponent should be >= 0');
+    return 'exponent should be >= 0';
   }
   if( exponent === 0){
     return base = 1;
   }
   const powerBase = base;
-  console.log(base * powerBase);
+  //console.log(base * powerBase);
 
   powerCalculator(base, exponent - 1);
-  // return base * powerCalculator(base, exponent -1 );
+  return base * powerCalculator(base, exponent -1 );
 }
 
-powerCalculator(10,3);
+const result = powerCalculator(10,-1);
+console.log(result);
 
 // 10 * 10 <== do that for each exponent
 // do the same thing, with expononet - 1
 // keep doing that until exponent === 0
 
-// 10 ^ 4 = 10000     10 * 1000     return 10 * RESULT
+// 10 ^ 4 = 10000     10 * 1000     return 10 * RESULT        return base * powerCalculator()
 // 10 ^ 3 = 1000      10 * 100      return 10 * RESULT
 // 10 ^ 2 = 100       10 * 10       return 10 * RESULT
 // 10 ^ 1 = 10        10 * 1        return 10 * RESULT
